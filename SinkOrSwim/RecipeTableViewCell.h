f3ba64dcb331564d2e15faf7182d7fc50adbacce
Lorsen <omarfroa@gmail.com>
2016-09-08 13:49:16 -0500

    Add RecipeTableViewCell Add Label to RecipeTableViewCell Add Image to RecipeTableViewCell

---
+++ SinkOrSwim/RecipeTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  RecipeTableViewCell.h
+//  SinkOrSwim
+//
+//  Created by Omar Roa on 9/8/16.
+//  Copyright © 2016 Jacob Adkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipeTableViewCell : UITableViewCell
+
+@end
