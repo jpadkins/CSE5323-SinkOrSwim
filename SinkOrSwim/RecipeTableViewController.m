0b4ebc90081717241ce721fb019f3b4cfe25392e
Lorsen <omarfroa@gmail.com>
2016-09-09 15:58:04 -0500

    Segmented Controller works (pastas and drinks).

--- SinkOrSwim/RecipeTableViewController.m
+++ SinkOrSwim/RecipeTableViewController.m
@@ -9,19 +9,80 @@
 #import "RecipeTableViewController.h"
 
 @interface RecipeTableViewController ()
-@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
 
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
 
 @end
 
 @implementation RecipeTableViewController
 
-NSArray *recipes;
+//NSArray *recipes;
+//
+//
+//- (void)viewDidLoad {
+//    [super viewDidLoad];
+//    recipes = [NSArray arrayWithObjects:@"Mac N Cheezi", @"Hot Linguini", @"Lemonady", nil];
+//}
+//
+//#pragma mark - Table view data source
+//
+//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+//    return 1;
+//}
+//
+//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+//    return [recipes count];
+//
+//}
+//
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    static NSString *simpleTableIdentifier = @"SimpleTableCell";
+//    
+//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+//    
+//    if (cell == nil) {
+//        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+//    }
+//    
+//    
+//    NSString *recipeName = [recipes objectAtIndex:indexPath.row];
+//    cell.textLabel.text = recipeName;
+//    cell.imageView.image = [UIImage imageNamed: recipeName];
+//    
+//    return cell;
+//}
+//
+//- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    if(indexPath.row == 0){
+//    [self performSegueWithIdentifier:@"MacNCheeziSegue" sender:self];
+//    }
+//    else if(indexPath.row == 1){
+//    [self performSegueWithIdentifier:@"HotLinguiniSegue" sender:self];
+//    }
+//    else if(indexPath.row == 2){
+//    [self performSegueWithIdentifier:@"LemonadyFreshNSqueezySegue" sender:self];
+//    }
+//}
+//- (IBAction)clickSegmentedController:(id)sender {
+//    
+//}
 
+
+NSArray *pastas;
+NSArray *drinks;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    recipes = [NSArray arrayWithObjects:@"Mac N Cheezi", @"Hot Linguini", @"Lemonady", nil];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    drinks = [NSArray arrayWithObjects:@"Lemonady", nil];
+    pastas = [NSArray arrayWithObjects:@"Mac N Cheezi", @"Hot Linguini", nil];
 }
 
 #pragma mark - Table view data source
@@ -30,26 +91,54 @@ NSArray *recipes;
     return 1;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [recipes count];
-
+-(NSInteger)tableView:(UITableView *)table numberOfRowsInSection:(NSInteger)section{
+    if(_segmentedControl.selectedSegmentIndex==0)
+    {
+        return [pastas count];
+    }
+    else if (_segmentedControl.selectedSegmentIndex==1)
+    {
+        return[drinks count];
+    }
+    return 0;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+-(IBAction) clickSegmentedController: (id)sender
+{
+    switch (self.segmentedControl.selectedSegmentIndex) {
+        case 0:
+            _segmentedControl.selectedSegmentIndex=0;
+              [self.tableView reloadData];
+            break;
+        case 1:
+            _segmentedControl.selectedSegmentIndex=1;
+              [self.tableView reloadData];
+        default:
+            break;
+    }
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *simpleTableIdentifier = @"SimpleTableCell";
-    
+
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
-    
+
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
     }
-    
-    
-    NSString *recipeName = [recipes objectAtIndex:indexPath.row];
-    cell.textLabel.text = recipeName;
-    cell.imageView.image = [UIImage imageNamed: recipeName];
-    
+
+    if(_segmentedControl.selectedSegmentIndex==0)
+    {
+
+        NSString *recipeName = [pastas objectAtIndex:indexPath.row];
+        cell.textLabel.text = recipeName;
+        cell.imageView.image = [UIImage imageNamed: recipeName];    }
+    else if (_segmentedControl.selectedSegmentIndex==1) {
+            NSString *recipeName = [drinks objectAtIndex:indexPath.row];
+            cell.textLabel.text = recipeName;
+            cell.imageView.image = [UIImage imageNamed: recipeName];
+        }
     return cell;
 }
 
@@ -65,74 +154,6 @@ NSArray *recipes;
     [self performSegueWithIdentifier:@"LemonadyFreshNSqueezySegue" sender:self];
     }
 }
-- (IBAction)clickSegmentedController:(id)sender {
-    
-}
-
 
 @end
 
-
-//NSArray *pastas;
-//NSArray *drinks;
-//
-//- (void)viewDidLoad {
-//    [super viewDidLoad];
-//    
-//    // Uncomment the following line to preserve selection between presentations.
-//    // self.clearsSelectionOnViewWillAppear = NO;
-//    
-//    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-//    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-//    drinks = [NSArray arrayWithObjects:@"Lemonady Fresh N Squeezy", nil];
-//    pastas = [NSArray arrayWithObjects:@"Mac N Cheezi", @"Hot Linguini", nil];
-//}
-//
-//#pragma mark - Table view data source
-//
-//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-//    return 1;
-//}
-//
-//-(NSInteger)tableView:(UITableView *)table numberOfRowsInSection:(NSInteger)section
-//{ if(segment.selectedSegmentIndex==0)
-//{
-//    return [pastas count];
-//}
-//else
-//    if (segment.selectedSegmentIndex==1) {
-//        return[drinks count];
-//
-//    }
-//
-//    return 0;
-//}
-//
-//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-//{
-//    static NSString *simpleTableIdentifier = @"SimpleTableCell";
-//
-//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
-//
-//    if (cell == nil) {
-//        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
-//    }
-//
-//
-//
-//    if(segment.selectedSegmentIndex==0)
-//    {
-//
-//        NSString *recipeName = [pastas objectAtIndex:indexPath.row];
-//        cell.textLabel.text = recipeName;
-//        cell.imageView.image = [UIImage imageNamed: recipeName];    }
-//    else if (segment.selectedSegmentIndex==1) {
-//            NSString *recipeName = [drinks objectAtIndex:indexPath.row];
-//            cell.textLabel.text = recipeName;
-//            cell.imageView.image = [UIImage imageNamed: recipeName];
-//        }
-//
-//
-//
-//    return cell;
-//}
