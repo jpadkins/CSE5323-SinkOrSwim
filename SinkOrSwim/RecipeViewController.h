272baf9c379f7503f592f8bba28167e2ee430fb2
jacobpadkins <jpadkins@smu.edu>
2016-09-09 20:12:51 -0500

    added missing files

---
+++ SinkOrSwim/RecipeViewController.h
@@ -0,0 +1,15 @@
+//
+//  RecipeViewController.h
+//  SinkOrSwim
+//
+//  Created by Omar Roa on 9/9/16.
+//  Copyright © 2016 Jacob Adkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipeViewController : UIViewController
+
+@property (strong, atomic) NSString *recipeType;
+
+@end
